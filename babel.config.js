b60dbb594d912d5bdfbcc6b1bf6b74cdeb50c4f6
jikwan <jikwan0327@gmail.com>
2023-01-06 10:00:53 +0900

    Edit Babel Setting

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ["@babel/preset-react", "@babel/preset-env"],
+  presets: [
+    ['@babel/preset-react', { targets: { browsers: ['last 2 versions', '>= 5% in KR'] } }],
+    '@babel/preset-env',
+    '@babel/typescript',
+  ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CreateDOM from 'react-dom/client';
-import { GlobalStyle } from 'style/globalStyle.style';
-import { baseTheme } from 'style/globalTheme.style';
+import { GlobalStyle } from './style/globalStyle.style';
+import { baseTheme } from './style/globalTheme.style';
 import { ThemeProvider } from 'styled-components';
 
 import App from './App';
