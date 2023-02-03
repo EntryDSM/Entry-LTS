e9a98b8f344e6b1032b5bda07f5a840109a34f22
jikwan <jikwan0327@gmail.com>
2023-02-03 15:17:00 +0900

    fix: webpack 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,58 @@
-const path = require("path");
+const path = require('path');
 
-const { CleanWebpackPlugin } = require("clean-webpack-plugin"); //추가
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.min.js",
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         // use: ['style-loader', 'css-loader'],
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.(jpeg|jpg|png)$/,
-        loader: "file-loader",
+        test: /\.(jpeg|jpg|png|svg)$/,
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]",
+          name: '[name].[ext]',
         },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, //바벨 로더가 실행시키지 못하도록 제외
+        loader: 'babel-loader', // 바벨 로더를 추가한다
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
-    new MiniCssExtractPlugin({ filename: "app.css" }),
+    new MiniCssExtractPlugin({ filename: 'app.css' }),
+    new ReactRefreshWebpackPlugin(),
   ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  },
+  watchOptions: {
+    poll: true,
+    ignored: '/node_modules/',
+  },
 };
