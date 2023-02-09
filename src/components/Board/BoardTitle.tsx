8c2bdc28bd2d6a31629bd0451066a1b8d5e5c442
Kim Jeong Hyun <kimjh11130@naver.com>
2023-02-09 20:30:14 +0900

    fix: 공지사항 디자인시스템 적용

--- src/components/Board/BoardTitle.tsx
+++ src/components/Board/BoardTitle.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { SetStateAction } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@team-entry/design_system';
+import { Button, Text } from '@team-entry/design_system';
 
 interface IBoardTitle {
   click: boolean;
@@ -17,22 +17,19 @@ interface IBoardTitle {
 
 const BoardTitle = (props: IBoardTitle) => {
   const { click, setClick, title, subTitle, button1, button2, button3, isCustomer, link } = props;
-
+  const onClick = () => {
+    console.log('clicked!');
+  };
   return (
     <>
-      <_Text fontSize={32} fontWeight={700}>
+      <Text color="black900" size="header1">
         {title}
-      </_Text>
-      <_Text
-        style={{ marginBottom: 22 }}
-        fontColor={isCustomer && '#737373'}
-        fontSize={isCustomer ? 20 : 18}
-        fontWeight={isCustomer ? 500 : 300}
-      >
+      </Text>
+      <Text margin={[0, 0, 22, 0]} color="realBlack" size="title3">
         {subTitle}
-      </_Text>
+      </Text>
       <_Buttons>
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', gap: '0.5rem' }}>
           <Button onClick={() => setClick(false)} color={'orange'} kind={click ? 'outlined' : 'contained'}>
             {button1}
           </Button>
@@ -43,7 +40,9 @@ const BoardTitle = (props: IBoardTitle) => {
         <div>
           {isCustomer && !click && (
             <Link to={link}>
-              <_Button isClick={true}>{button3}</_Button>
+              <Button color="orange" onClick={onClick}>
+                {button3}
+              </Button>
             </Link>
           )}
         </div>
@@ -53,34 +52,6 @@ const BoardTitle = (props: IBoardTitle) => {
 };
 
 export default BoardTitle;
-
-const _Text = styled.div<{ fontSize: number; fontWeight: number; fontColor?: string; cursor?: boolean }>`
-  color: ${(props) => props.fontColor};
-  font-size: ${(props) => props.fontSize}px;
-  font-weight: ${(props) => props.fontWeight};
-  cursor: ${(props) => (props.cursor ? 'pointer' : 'auto')};
-`;
-
-const _Button = styled.button<{ isClick: boolean }>`
-  padding: 12px;
-  outline: 0;
-  border: 0;
-  border-radius: 5px;
-  margin-right: 10px;
-  font-size: 16px;
-  font-weight: 500;
-  color: ${(props) => (props.isClick ? 'white' : '#ff7e36')};
-  background-color: ${(props) => (props.isClick ? '#ff7e36' : 'white')};
-  border: 1px solid #ff7e36;
-  cursor: pointer;
-  &:hover {
-    background-color: #ff7e36;
-    color: white;
-  }
-  &:last-child {
-    margin: 0;
-  }
-`;
 
 const _Buttons = styled.div`
   display: flex;
