47522e7cb7d5a10b1b19f3012245b9ca077cea5e
jikwan <jikwan0327@gmail.com>
2023-04-05 08:48:32 +0900

    feat: env 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-  entry: './src/index.tsx',
+  entry: './src/index',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
@@ -15,6 +20,7 @@ module.exports = {
   output: {
     filename: 'bundle.min.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -43,9 +49,18 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.EnvironmentPlugin(['REACT_APP_BASE_URL']),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_BASE_URL': JSON.stringify(process.env.REACT_APP_BASE_URL),
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js',
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: path.resolve('public/index.html'),
+      filename: 'index.html',
+      inject: true,
     }),
     new MiniCssExtractPlugin({ filename: 'app.css' }),
     new ReactRefreshWebpackPlugin(),
