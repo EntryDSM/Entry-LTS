7c895c601fba6aac9173747ecd284fc573304111
jikwan <jikwan0327@gmail.com>
2023-07-11 18:40:05 +0900

    feat: CD

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.min.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
   },
   module: {
