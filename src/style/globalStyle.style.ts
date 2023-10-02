84ac85b0ef5f9e99835ba106ab3118856f461142
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-02 16:31:47 +0900

    fix: font local 삭제

--- src/style/globalStyle.style.ts
+++ src/style/globalStyle.style.ts
@@ -5,24 +5,23 @@ export const GlobalStyle = css`
     font-family: 'Pretendard';
     font-weight: 700;
     font-style: bold;
-    src: local('Pretendard')
-      url('https://s3.ap-northeast-2.amazonaws.com/rolls-image.entry.com/font/Pretendard-Bold.woff2') format('woff2');
+    src: url('https://s3.ap-northeast-2.amazonaws.com/rolls-image.entry.com/font/Pretendard-Bold.woff2') format('woff2');
   }
 
   @font-face {
     font-family: 'Pretendard';
     font-weight: 500;
     font-style: medium;
-    src: local('Pretendard')
-      url('https://s3.ap-northeast-2.amazonaws.com/rolls-image.entry.com/font/Pretendard-Medium.woff2') format('woff2');
+    src: url('https://s3.ap-northeast-2.amazonaws.com/rolls-image.entry.com/font/Pretendard-Medium.woff2')
+      format('woff2');
   }
 
   @font-face {
     font-family: 'Pretendard';
     font-weight: 400;
     font-style: regular;
-    src: local('Pretendard')
-      url('https://s3.ap-northeast-2.amazonaws.com/rolls-image.entry.com/font/Pretendard-Regular.woff2') format('woff2');
+    src: url('https://s3.ap-northeast-2.amazonaws.com/rolls-image.entry.com/font/Pretendard-Regular.woff2')
+      format('woff2');
   }
 
   html,
