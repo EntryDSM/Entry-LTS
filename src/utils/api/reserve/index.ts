cf6c575e446093f5cdc6e0b8b6bcf7afc0d0e66d
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-04 09:47:02 +0900

    fix: reserve 링크 수정

--- src/utils/api/reserve/index.ts
+++ src/utils/api/reserve/index.ts
@@ -1,6 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { instance } from '../axios';
 import { IReserveResponse } from './types';
+import { AxiosError } from 'axios';
+import { Toast } from '@team-entry/design_system';
 
 const router = `/reserve`;
 
@@ -9,5 +11,12 @@ export const GetReserveLink = () => {
     const { data } = await instance.get<IReserveResponse>(`${router}`);
     return data;
   };
-  return useQuery(['reserve'], response);
+  return useMutation(response, {
+    onSuccess: (res) => {
+      if (!!res.reserve_link) window.open(res.reserve_link.toString());
+    },
+    onError: () => {
+      Toast('지금은 불가능합니다.', { type: 'error' });
+    },
+  });
 };
