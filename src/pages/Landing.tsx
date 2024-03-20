fd8cc1a64fbfb2109346f4de48527322b97c2a63
ojinikim <ojinikim@gmail.com>
2024-03-20 11:27:22 +0900

    style :: 랜딩페이지 합침

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,19 @@
+import First from '@/components/landing/FirstContainer';
+import Second from '@/components/landing/SecondContainer';
 import Third from '@/components/landing/ThirdContainer';
 import Fourth from '@/components/landing/FourthContainer';
+import Fifth from '@/components/landing/FifthContainer';
+import Sixth from '@/components/landing/SixthContainer';
 
 const Landing = () => {
   return (
     <>
+      <First />
+      <Second />
       <Third />
       <Fourth />
+      <Fifth />
+      <Sixth />
     </>
   );
 };
