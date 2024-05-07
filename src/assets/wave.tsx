69498de78b7b424d348e72f5e16e0394572dc0d1
wlals <jiminelp@gmail.com>
2024-05-07 21:28:45 +0900

    style :: svg태그 속성 수정

--- src/assets/wave.tsx
+++ src/assets/wave.tsx
@@ -6,7 +6,7 @@ const Wave = () => {
       xmlnsXlink="http://www.w3.org/1999/xlink"
       viewBox="0 24 150 28"
       preserveAspectRatio="none"
-      shape-rendering="auto"
+      shapeRendering="auto"
     >
       <defs>
         <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
