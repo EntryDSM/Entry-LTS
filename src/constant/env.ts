b77512c5f90d064bc59352897614cf8caa6b4d51
dutexion <dutexion@gmail.com>
2024-06-10 17:23:09 +0900

    chore :: 환경변수

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,9 +1,9 @@
 export const COOKIE_DOMAIN = process.env.NODE_ENV === 'development' ? 'localhost' : 'entrydsm.hs.kr';
 
-export const AUTH_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://auth.entrydsm.hs.kr';
+export const MAIN_URL = process.env.REACT_APP_MAIN_URL;
 
-export const APPLY_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://apply.entrydsm.hs.kr';
+export const AUTH_URL = process.env.REACT_APP_AUTH_URL;
 
-export const MAIN_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : 'https://www.entrydsm.hs.kr';
+export const APPLY_URL = process.env.REACT_APP_APPLY_URL;
+
+export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL;
