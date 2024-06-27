d39c2e270cd955871f12de96a258082040e5caba
dutexion <dutexion@gmail.com>
2024-06-27 14:52:21 +0900

    rollback

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import { RouterProvider } from 'react-router-dom';
 import Router from './router';
 import { Global } from '@emotion/react';
 import { GlobalStyle } from './style/globalStyle.style';
-import { AUTH_URL } from './constant/env';
 
 function App() {
-  console.log(AUTH_URL, process.env.REACT_SERVER_BASE_URL);
-
   return (
     <>
       <Global styles={GlobalStyle} />
