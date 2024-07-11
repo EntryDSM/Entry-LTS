d3f79c2a20426a242dbfa666acfff37ef53de24d
dutexion <dutexion@gmail.com>
2024-07-11 11:56:17 +0900

    chore :: 환경변수 이름 변경

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import * as ChannelService from '@channel.io/channel-web-sdk-loader';
 ChannelService.loadScript();
 
 ChannelService.boot({
-  pluginKey: `${process.env.CHANNEL_TALK_PLUGIN_KEY}`,
+  pluginKey: `${process.env.REACT_APP_CHANNEL_TALK_PLUGIN_KEY}`,
 });
 
 const root = CreateDOM.createRoot(document.getElementById('root') as HTMLElement);
