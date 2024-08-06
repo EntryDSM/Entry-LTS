b48f3300982003be2ee90a2f161934bc07116478
dutexion <dutexion@gmail.com>
2024-08-06 15:40:17 +0900

    배포 시도 (vite config ts)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import path from 'path';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  esbuild: {
+    supported: {
+      'top-level-await': true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        {
+          name: 'ignore-use-client-directive',
+          setup(build) {
+            build.onLoad({ filter: /.*/ }, async (args) => {
+              if (args.path.includes('node_modules/@tanstack/react-query-devtools')) {
+                return {
+                  contents: (await fs.promises.readFile(args.path, 'utf8')).replace(/'use client';?/g, ''),
+                };
+              }
+            });
+          },
+        },
+      ],
+    },
+  },
   server: {
     port: 3002,
   },
