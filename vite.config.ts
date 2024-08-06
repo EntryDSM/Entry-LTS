35978c5bf299e5e68a4c8da54030619445ec3d1c
dutexion <dutexion@gmail.com>
2024-08-06 20:23:27 +0900

    chunk 쪼개기

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,15 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
   },
   plugins: [react(), reactRefresh(), removeUseClientPlugin()],
 });
