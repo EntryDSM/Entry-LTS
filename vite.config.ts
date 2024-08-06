b2bb2007d72512b4408673bf4b4e0f84f73748ad
dutexion <dutexion@gmail.com>
2024-08-06 20:28:00 +0900

    chunk 늘리기

--- vite.config.ts
+++ vite.config.ts
@@ -35,15 +35,16 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 1000,
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //       }
+    //     },
+    //   },
+    // },
   },
   plugins: [react(), reactRefresh(), removeUseClientPlugin()],
 });
