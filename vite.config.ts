698473df7ad48282d00d1e52ba24807e89f8c38e
dutexion <dutexion@gmail.com>
2024-08-06 20:38:03 +0900

    chunk 더 늘리기

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 3500,
     // rollupOptions: {
     //   output: {
     //     manualChunks(id) {
