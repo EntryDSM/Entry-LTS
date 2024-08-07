84c8a08fef942b7cc752edfc31004eeebf9dccbb
HyunSu1768 <azxcv1768@gmail.com>
2024-08-07 15:25:25 +0900

    fix :: env 수정

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,9 +1,11 @@
-aexport const COOKIE_DOMAIN = process.env.NODE_ENV === 'development' ? 'localhost' : 'entrydsm.hs.kr';
+import process from 'process';
 
-export const MAIN_URL = window.ENV?.VITE_MAIN_URL || import.meta.env.VITE_MAIN_URL;
+export const COOKIE_DOMAIN = process.env.NODE_ENV === 'development' ? 'localhost' : 'entrydsm.hs.kr';
 
-export const AUTH_URL = window.ENV?.VITE_AUTH_URL || import.meta.env.VITE_AUTH_URL;
+export const MAIN_URL = import.meta.env.VITE_MAIN_URL;
 
-export const APPLY_URL = window.ENV?.VITE_APPLY_URL || import.meta.env.VITE_APPLY_URL;
+export const AUTH_URL = import.meta.env.VITE_AUTH_URL;
 
-export const ADMIN_URL = window.ENV?.VITE_ADMIN_URL || import.meta.env.VITE_ADMIN_URL;
+export const APPLY_URL = import.meta.env.VITE_APPLY_URL;
+
+export const ADMIN_URL = import.meta.env.VITE_ADMIN_URL;
